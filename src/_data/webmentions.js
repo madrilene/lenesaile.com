7b44b8b7d30fb0f39ceedee866a436038252c7ed
madrilene <hola@lenesaile.com>
2023-01-02 23:00:08 +0100

    webmentions sorting sorted out

--- src/_data/webmentions.js
+++ src/_data/webmentions.js
@@ -12,36 +12,89 @@ const API = 'https://webmention.io/api';
 const TOKEN = process.env.WEBMENTION_IO_TOKEN;
 
 async function fetchWebmentions(since, perPage = 10000) {
-  if (!domain) {
-    // If we dont have a domain name, abort
-    console.warn('>>> unable to fetch webmentions: no domain name specified in meta.js');
-    return false;
-  }
-
-  if (!TOKEN) {
-    // If we dont have a domain access token, abort
-    console.warn(
-      '>>> unable to fetch webmentions: no access token specified in environment.'
-    );
+  // If we dont have a domain name or token, abort
+  if (!domain || !TOKEN) {
+    console.warn('>>> unable to fetch webmentions: missing domain or token');
     return false;
   }
 
   let url = `${API}/mentions.jf2?domain=${domain}&token=${TOKEN}&per-page=${perPage}`;
-  if (since) url += `&since=${since}`;
+  if (since) url += `&since=${since}`; // only fetch new mentions
 
   const response = await fetch(url);
-  if (response.ok) {
-    const feed = await response.json();
-    console.log(`>>> ${feed.children.length} new webmentions fetched from ${API}`);
-    return feed;
+  if (!response.ok) {
+    return null;
   }
+  const feed = await response.json();
+  const webmentions = feed.children;
+  let cleanedWebmentions = cleanWebmentions(webmentions);
+  if (cleanedWebmentions.length === 0) {
+    console.log('[Webmention] No new webmention');
+    return null;
+  } else {
+    console.log(`[Webmention] ${cleanedWebmentions.length} new webmentions`);
+    return cleanedWebmentions;
+  }
+}
 
-  return null;
+function cleanWebmentions(webmentions) {
+  // https://mxb.dev/blog/using-webmentions-on-static-sites/#h-parsing-and-filtering
+  const sanitize = entry => {
+    // Sanitize HTML content
+    const {content} = entry;
+    if (content && content['content-type'] === 'text/html') {
+      let html = content.html;
+      html = html
+        .replace(/<a [^>]+><\/a>/gm, '')
+        .trim()
+        .replace(/\n/g, '<br />');
+      html = sanitizeHTML(html, {
+        allowedTags: [
+          'b',
+          'i',
+          'em',
+          'strong',
+          'a',
+          'blockquote',
+          'ul',
+          'ol',
+          'li',
+          'code',
+          'pre',
+          'br'
+        ],
+        allowedAttributes: {
+          a: ['href', 'rel'],
+          img: ['src', 'alt']
+        },
+        allowedIframeHostnames: []
+      });
+      content.html = html;
+    }
+
+    // Fix missing publication date
+    if (!entry.published && entry['wm-received']) {
+      entry.published = entry['wm-received'];
+    }
+
+    return entry;
+  };
+
+  return webmentions.map(sanitize);
 }
 
 // Merge fresh webmentions with cached entries, unique per id
 function mergeWebmentions(a, b) {
-  return unionBy(a.children, b.children, 'wm-id');
+  if (b.length === 0) {
+    return a;
+  }
+  let union = unionBy(a, b, 'wm-id');
+  union.sort((a, b) => {
+    let aDate = new Date(a.published || a['wm-received']);
+    let bDate = new Date(b.published || b['wm-received']);
+    return aDate - bDate;
+  });
+  return union;
 }
 
 // save combined webmentions in cache file
