0e583a7abd8d73a59a2124824938c22bb39264f6
madrilene <hola@lenesaile.com>
2023-11-05 19:36:08 +0100

    back to regular fn

--- src/assets/scripts/hydrated/gsap.js
+++ src/assets/scripts/hydrated/gsap.js
@@ -3,19 +3,17 @@
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 
-export function initGsapScroll() {
-  const marks = document.querySelectorAll('mark');
-  marks.forEach(element => {
-    element.classList.add('gsap-highlight');
-  });
+const marks = document.querySelectorAll('mark');
+marks.forEach(element => {
+  element.classList.add('gsap-highlight');
+});
 
-  gsap.registerPlugin(ScrollTrigger);
+gsap.registerPlugin(ScrollTrigger);
 
-  gsap.utils.toArray('.gsap-highlight').forEach(highlight => {
-    ScrollTrigger.create({
-      trigger: highlight,
-      start: '-100px center',
-      onEnter: () => highlight.classList.add('active')
-    });
+gsap.utils.toArray('.gsap-highlight').forEach(highlight => {
+  ScrollTrigger.create({
+    trigger: highlight,
+    start: '-100px center',
+    onEnter: () => highlight.classList.add('active')
   });
-}
+});
--- src/assets/scripts/hydrated/masonry.js
+++ src/assets/scripts/hydrated/masonry.js
@@ -1,50 +1,48 @@
 // ----- masonry fallback if CSS masonry not supported, solution by Ana Tudor: https://codepen.io/thebabydino/pen/yLYppjK
 
-export function initMasonryFallback() {
-  const supportMasonry = CSS.supports('grid-template-rows', 'masonry');
+const supportMasonry = CSS.supports('grid-template-rows', 'masonry');
 
-  if (!supportMasonry) {
-    let grids = [...document.querySelectorAll('.grid[data-rows="masonry"]')];
+if (!supportMasonry) {
+  let grids = [...document.querySelectorAll('.grid[data-rows="masonry"]')];
 
-    if (grids.length && getComputedStyle(grids[0]).gridTemplateRows !== 'masonry') {
-      grids = grids.map(grid => ({
-        _el: grid,
-        gap: parseFloat(getComputedStyle(grid).rowGap),
-        items: [...grid.childNodes]
-          .filter(c => c.nodeType === 1 && +getComputedStyle(c).gridColumnEnd !== -1)
-          .map(c => ({_el: c})),
-        ncol: 0
-      }));
+  if (grids.length && getComputedStyle(grids[0]).gridTemplateRows !== 'masonry') {
+    grids = grids.map(grid => ({
+      _el: grid,
+      gap: parseFloat(getComputedStyle(grid).rowGap),
+      items: [...grid.childNodes]
+        .filter(c => c.nodeType === 1 && +getComputedStyle(c).gridColumnEnd !== -1)
+        .map(c => ({_el: c})),
+      ncol: 0
+    }));
 
-      function layout() {
-        grids.forEach(grid => {
-          let ncol = getComputedStyle(grid._el).gridTemplateColumns.split(' ').length;
+    function layout() {
+      grids.forEach(grid => {
+        let ncol = getComputedStyle(grid._el).gridTemplateColumns.split(' ').length;
 
-          if (grid.ncol !== ncol) {
-            grid.ncol = ncol;
+        if (grid.ncol !== ncol) {
+          grid.ncol = ncol;
 
-            grid.items.forEach(c => c._el.style.removeProperty('margin-block-start'));
+          grid.items.forEach(c => c._el.style.removeProperty('margin-block-start'));
 
-            if (grid.ncol > 1) {
-              grid.items.slice(ncol).forEach((c, i) => {
-                let prev_fin = grid.items[i]._el.getBoundingClientRect().bottom,
-                  curr_ini = c._el.getBoundingClientRect().top;
+          if (grid.ncol > 1) {
+            grid.items.slice(ncol).forEach((c, i) => {
+              let prev_fin = grid.items[i]._el.getBoundingClientRect().bottom,
+                curr_ini = c._el.getBoundingClientRect().top;
 
-                c._el.style.marginTop = `${prev_fin + grid.gap - curr_ini}px`;
-              });
-            }
+              c._el.style.marginTop = `${prev_fin + grid.gap - curr_ini}px`;
+            });
           }
-        });
-      }
-
-      addEventListener(
-        'load',
-        e => {
-          layout();
-          addEventListener('resize', layout, false);
-        },
-        false
-      );
+        }
+      });
     }
+
+    addEventListener(
+      'load',
+      e => {
+        layout();
+        addEventListener('resize', layout, false);
+      },
+      false
+    );
   }
 }
