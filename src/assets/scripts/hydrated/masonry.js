51429315d720ef84eeedbdac957d9ca63d295c6e
madrilene <hola@lenesaile.com>
2023-11-05 12:22:34 +0100

    no comments, ES module

--- src/assets/scripts/hydrated/masonry.js
+++ src/assets/scripts/hydrated/masonry.js
@@ -1,56 +1,50 @@
 // ----- masonry fallback if CSS masonry not supported, solution by Ana Tudor: https://codepen.io/thebabydino/pen/yLYppjK
 
-const supportMasonry = CSS.supports('grid-template-rows', 'masonry');
+export function initMasonryFallback() {
+  const supportMasonry = CSS.supports('grid-template-rows', 'masonry');
 
-if (!supportMasonry) {
-  let grids = [...document.querySelectorAll('.grid[data-rows="masonry"]')];
+  if (!supportMasonry) {
+    let grids = [...document.querySelectorAll('.grid[data-rows="masonry"]')];
 
-  if (grids.length && getComputedStyle(grids[0]).gridTemplateRows !== 'masonry') {
-    grids = grids.map(grid => ({
-      _el: grid,
-      gap: parseFloat(getComputedStyle(grid).gridRowGap),
-      items: [...grid.childNodes]
-        .filter(c => c.nodeType === 1 && +getComputedStyle(c).gridColumnEnd !== -1)
-        .map(c => ({_el: c})),
-      ncol: 0
-    }));
+    if (grids.length && getComputedStyle(grids[0]).gridTemplateRows !== 'masonry') {
+      grids = grids.map(grid => ({
+        _el: grid,
+        gap: parseFloat(getComputedStyle(grid).rowGap),
+        items: [...grid.childNodes]
+          .filter(c => c.nodeType === 1 && +getComputedStyle(c).gridColumnEnd !== -1)
+          .map(c => ({_el: c})),
+        ncol: 0
+      }));
 
-    function layout() {
-      grids.forEach(grid => {
-        /* get the post relayout number of columns */
-        let ncol = getComputedStyle(grid._el).gridTemplateColumns.split(' ').length;
+      function layout() {
+        grids.forEach(grid => {
+          let ncol = getComputedStyle(grid._el).gridTemplateColumns.split(' ').length;
 
-        /* if the number of columns has changed */
-        if (grid.ncol !== ncol) {
-          /* update number of columns */
-          grid.ncol = ncol;
+          if (grid.ncol !== ncol) {
+            grid.ncol = ncol;
 
-          /* revert to initial positioning, no margin */
-          grid.items.forEach(c => c._el.style.removeProperty('margin-block-start'));
+            grid.items.forEach(c => c._el.style.removeProperty('margin-block-start'));
 
-          /* if we have more than one column */
-          if (grid.ncol > 1) {
-            grid.items.slice(ncol).forEach((c, i) => {
-              let prev_fin =
-                  grid.items[i]._el.getBoundingClientRect()
-                    .bottom /* bottom edge of item above */,
-                curr_ini =
-                  c._el.getBoundingClientRect().top; /* top edge of current item */
+            if (grid.ncol > 1) {
+              grid.items.slice(ncol).forEach((c, i) => {
+                let prev_fin = grid.items[i]._el.getBoundingClientRect().bottom,
+                  curr_ini = c._el.getBoundingClientRect().top;
 
-              c._el.style.marginTop = `${prev_fin + grid.gap - curr_ini}px`;
-            });
+                c._el.style.marginTop = `${prev_fin + grid.gap - curr_ini}px`;
+              });
+            }
           }
-        }
-      });
-    }
+        });
+      }
 
-    addEventListener(
-      'load',
-      e => {
-        layout(); /* initial load */
-        addEventListener('resize', layout, false); /* on resize */
-      },
-      false
-    );
+      addEventListener(
+        'load',
+        e => {
+          layout();
+          addEventListener('resize', layout, false);
+        },
+        false
+      );
+    }
   }
 }
