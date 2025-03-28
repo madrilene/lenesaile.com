3b4ca429c78e00f2f5ce611cebd5830e6d070ec4
madrilene <hola@lenesaile.com>
2025-03-28 11:20:22 +0100

    update posts coll

--- src/_config/collections.js
+++ src/_config/collections.js
@@ -1,6 +1,6 @@
-/** All blog posts as a collection. */
+/** All blog posts as a collection, by language */
 export const postsByLang = (collection, lang) => {
-  return collection.getFilteredByGlob(`./src/posts/${lang}/**/*.md`).reverse();
+  return collection.getFilteredByGlob(`./src/content/posts/${lang}/**/*.md`).reverse();
 };
 
 /** All relevant pages as a collection for sitemap.xml */
@@ -13,7 +13,7 @@ export const tagList = collection => {
   const tagsSet = new Set();
   collection.getAll().forEach(item => {
     if (!item.data.tags) return;
-    item.data.tags.filter(tag => !['posts', 'docs', 'all'].includes(tag)).forEach(tag => tagsSet.add(tag));
+    item.data.tags.filter(tag => !['posts', 'all'].includes(tag)).forEach(tag => tagsSet.add(tag));
   });
   return Array.from(tagsSet).sort();
 };
