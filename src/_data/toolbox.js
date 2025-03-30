9b4f9242da4ca45a05f756f0ca3e27381749dbdc
madrilene <hola@lenesaile.com>
2025-03-30 11:41:00 +0200

    build services

---
+++ src/_data/toolbox.js
@@ -0,0 +1,136 @@
+export default {
+  en: [
+    {
+      key: 'consulting',
+      title: 'Consulting',
+      summary:
+        'I offer expert guidance to create visually appealing websites, tailored to you personally, giving your visitors a coherent impression of you and what you offer.',
+      link: ''
+    },
+    {
+      key: 'development',
+      title: 'Development',
+      summary:
+        'Modern web frameworks like Eleventy and Astro help me to create websites based on semantic HTML, CSS and JavaScript without the need for databases or server-side languages.',
+      link: ''
+    },
+    {
+      key: 'design',
+      title: 'Design',
+      summary:
+        'I specialize in creative web design, creating visually stunning experiences: Elegant, responsive websites that are fun and easy to use.',
+      link: ''
+    },
+    {
+      key: 'maintenance',
+      title: 'Maintenance',
+      summary:
+        'The internet constantly evolves and so do the tools we use to build websites. I make sure that my websites function optimally and are secure over the long term.',
+      link: ''
+    },
+    {
+      key: 'discoverability',
+      title: 'Discoverability',
+      summary:
+        'The secret for discoverability is to have a website that is great in every way: Technically perfect, fast and secure, clear in terms of structure, with genuinely good copy. I can help you with that.',
+      link: ''
+    },
+    {
+      key: 'contents',
+      title: 'Contents',
+      summary:
+        "Let's try to find what it is that differentiates you. Something personal, that refers to the essence or philosophy of the project or product.",
+      link: ''
+    }
+  ],
+
+  de: [
+    {
+      key: 'consulting',
+      title: 'Consulting',
+      summary:
+        'Ich entwickle gemeinsam mit dir visuell ansprechende Websites, die auf dich persönlich zugeschnitten sind und deinen Besuchern einen stimmigen Eindruck von dir und deinem Angebot vermitteln.',
+      link: ''
+    },
+    {
+      key: 'development',
+      title: 'Development',
+      summary:
+        'Moderne Web-Frameworks wie Eleventy und Astro helfen mir, Websites auf der Grundlage von semantischem HTML, CSS und JavaScript zu erstellen.',
+      link: ''
+    },
+    {
+      key: 'design',
+      title: 'Design',
+      summary:
+        'Ich habe mich auf kreatives Webdesign spezialisiert. Visuell spannende Websites, die Spaß machen, und einfach zu bedienen sind.',
+      link: ''
+    },
+    {
+      key: 'maintenance',
+      title: 'Wartung',
+      summary:
+        'Das Internet entwickelt sich ständig weiter und damit auch die Tools, die wir zur Erstellung von Websites verwenden. Ich kümmere mich darum, dass meine Websites langanhaltend optimal funktionieren und sicher sind.',
+      link: ''
+    },
+    {
+      key: 'discoverability',
+      title: 'Auffindbarkeit',
+      summary:
+        'Das Geheimnis für Auffindbarkeit ist eine Website, die in jeder Hinsicht gut ist: Technisch perfekt, schnell und sicher, semantisch strukturiert, mit ehrlich guten Inhalten.',
+      link: ''
+    },
+    {
+      key: 'contents',
+      title: 'Inhalte',
+      summary:
+        'Versuchen wir herauszufinden, was dich von anderen unterscheidet. Etwas Persönliches, das sich auf die Essenz oder die Philosophie des Projekts oder Produkts bezieht.',
+      link: ''
+    }
+  ],
+
+  es: [
+    {
+      key: 'consulting',
+      title: 'Consulting',
+      summary:
+        'Ofrezco asesoramiento experto para crear sitios web visualmente atrayentes, adaptados a ti personalmente, que den a tus visitantes una impresión coherente de ti y de lo que ofreces.',
+      link: ''
+    },
+    {
+      key: 'development',
+      title: 'Desarrollo',
+      summary:
+        'Web frameworks como Eleventy y Astro me ayudan a crear sitios web basados en HTML semántico, CSS y JavaScript sin necesidad de bases de datos ni lenguajes del lado del servidor.',
+      link: ''
+    },
+    {
+      key: 'design',
+      title: 'Diseño',
+      summary:
+        'Me especializo en diseño web creativo, creando experiencias visualmente impresionantes: Páginas web elegantes, responsivos, divertidos y fáciles de usar.',
+      link: ''
+    },
+    {
+      key: 'maintenance',
+      title: 'Mantenimiento',
+      summary:
+        'El internet evoluciona constantemente y también lo hacen las herramientas que utilizamos. Me aseguro de que mis páginas web funcionen de forma óptima y sean seguras a largo plazo.',
+      link: ''
+    },
+    {
+      key: 'discoverability',
+      title: 'Visibilidad',
+      summary:
+        'El secreto de una web descubrible, es que sea genial en todos los sentidos: Técnicamente perfecta, rápida y segura, semanticamente estructurada, con contenidos realmente buenos.',
+      link: ''
+    },
+    {
+      key: 'contents',
+      title: 'Contenidos',
+      summary:
+        'Intentemos encontrar qué es lo que te diferencia. Algo personal, que haga referencia a la esencia o filosofía del proyecto o producto.',
+      link: ''
+    }
+  ]
+};
