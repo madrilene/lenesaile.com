edf2fbceedb037514f668705311f80d09949cee8
madrilene <hola@lenesaile.com>
2025-04-10 12:55:44 +0200

    hack to get blog og image

---
+++ src/_data/ogBlogPages.js
@@ -0,0 +1,17 @@
+export default [
+  {
+    lang: 'en',
+    title: 'Blog',
+    description: "I'm writing about things related to web development. Projects, approaches and observations, things I have learned or consider important."
+  },
+  {
+    lang: 'de',
+    title: 'Blog',
+    description: 'Ich schreibe vor allem über Dinge, die mit Webentwicklung zu tun haben. Projekte, Ansätze und Beobachtungen, Dinge, die ich gelernt habe oder für wichtig halte.'
+  },
+  {
+    lang: 'es',
+    title: 'Blog',
+    description: 'Escribo sobre cosas relacionadas con el desarrollo web. Proyectos, enfoques y observaciones, cosas que he aprendido o que considero importantes.'
+  }
+]
