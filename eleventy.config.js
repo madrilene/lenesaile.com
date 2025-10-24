madrilene/eleventy-excellent

feat: add imageKeys shortcode

eleventy.config.js
@@ -89,6 +89,7 @@ export default async function (eleventyConfig) {
   // --------------------- Shortcodes
   eleventyConfig.addShortcode('svg', shortcodes.svgShortcode);
   eleventyConfig.addShortcode('image', shortcodes.imageShortcode);
+  eleventyConfig.addShortcode('imageKeys', shortcodes.imageKeysShortcode);
   eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
 
   // --------------------- Events: after build

src/_config/shortcodes.js
@@ -1,4 +1,4 @@
-import {imageShortcode} from './shortcodes/image.js';
+import {imageShortcode, imageKeysShortcode} from './shortcodes/image.js';
 import {svgShortcode} from './shortcodes/svg.js';
 
-export default {imageShortcode, svgShortcode};
+export default {imageShortcode, imageKeysShortcode, svgShortcode};

src/_config/shortcodes/image.js
@@ -10,17 +10,24 @@ const stringifyAttributes = attributeMap => {
     .join(' ');
 };
 
-export const imageShortcode = async (
-  src,
-  alt = '',
-  caption = '',
-  loading = 'lazy',
-  containerClass,
-  imageClass,
-  widths = [650, 960, 1400],
-  sizes = 'auto',
-  formats = ['avif', 'webp', 'jpeg']
-) => {
+const errorSrcRequired = shortcodeName => {
+  throw new Error(`src parameter is required for {% ${shortcodeName} %} shortcode`);
+};
+
+// Handles image processing
+const processImage = async options => {
+  let {
+    src,
+    alt = '',
+    caption = '',
+    loading = 'lazy',
+    containerClass,
+    imageClass,
+    widths = [650, 960, 1400],
+    sizes = 'auto',
+    formats = ['avif', 'webp', 'jpeg']
+  } = options;
+
   // Prepend "./src" if not present
   if (!src.startsWith('./src')) {
     src = `./src${src}`;
@@ -64,4 +71,40 @@ export const imageShortcode = async (
   return caption
     ? `<figure slot="image"${containerClass ? ` class="${containerClass}"` : ''}>${pictureElement}<figcaption>${caption}</figcaption></figure>`
     : `<picture slot="image"${containerClass ? ` class="${containerClass}"` : ''}>${imageSources}<img ${imageAttributes}></picture>`;
+};
+
+// Positional parameters (legacy)
+export const imageShortcode = async (
+  src,
+  alt,
+  caption,
+  loading,
+  containerClass,
+  imageClass,
+  widths,
+  sizes,
+  formats
+) => {
+  if (!src) {
+    errorSrcRequired('image');
+  }
+  return processImage({
+    src,
+    alt,
+    caption,
+    loading,
+    containerClass,
+    imageClass,
+    widths,
+    sizes,
+    formats
+  });
+};
+
+// Named parameters
+export const imageKeysShortcode = async (options = {}) => {
+  if (!options.src) {
+    errorSrcRequired('imageKeys');
+  }
+  return processImage(options);
 };

madrilene/lenesaile.com

eleventy.config.js
@@ -119,6 +119,7 @@ export default async function (eleventyConfig) {
   eleventyConfig.addPairedShortcode('asideReadmore', shortcodes.asideReadmore);
   eleventyConfig.addShortcode('svg', shortcodes.svg);
   eleventyConfig.addShortcode('image', shortcodes.image);
+  eleventyConfig.addShortcode('imageKeys', shortcodes.imageKeys);
   eleventyConfig.addShortcode('year', () => `${new Date().getFullYear()}`);
 
   // --------------------- Events: after build

src/_config/shortcodes.js
@@ -1,6 +1,6 @@
 import {asideInfo} from './shortcodes/aside-info.js';
 import {asideReadmore} from './shortcodes/aside-readmore.js';
-import {image} from './shortcodes/image.js';
+import {image, imageKeys} from './shortcodes/image.js';
 import {svg} from './shortcodes/svg.js';
 
-export default {asideInfo, asideReadmore, image, svg};
+export default {asideInfo, asideReadmore, image, imageKeys, svg};

src/_config/shortcodes/image.js
@@ -10,17 +10,24 @@ const stringifyAttributes = attributeMap => {
     .join(' ');
 };
 
-export const image = async (
-  src,
-  alt = '',
-  caption = '',
-  loading = 'lazy',
-  containerClass,
-  imageClass,
-  widths = [650, 960, 1400],
-  sizes = 'auto',
-  formats = ['webp', 'jpeg']
-) => {
+const errorSrcRequired = shortcodeName => {
+  throw new Error(`src parameter is required for {% ${shortcodeName} %} shortcode`);
+};
+
+// Handles image processing
+const processImage = async options => {
+  let {
+    src,
+    alt = '',
+    caption = '',
+    loading = 'lazy',
+    containerClass,
+    imageClass,
+    widths = [650, 960, 1400],
+    sizes = 'auto',
+    formats = ['avif', 'webp', 'jpeg']
+  } = options;
+
   // Prepend "./src" if not present
   if (!src.startsWith('./src')) {
     src = `./src${src}`;
@@ -64,4 +71,40 @@ export const image = async (
   return caption
     ? `<figure slot="image"${containerClass ? ` class="${containerClass}"` : ''}>${pictureElement}<figcaption>${caption}</figcaption></figure>`
     : `<picture slot="image"${containerClass ? ` class="${containerClass}"` : ''}>${imageSources}<img ${imageAttributes}></picture>`;
+};
+
+// Positional parameters (legacy)
+export const image = async (
+  src,
+  alt,
+  caption,
+  loading,
+  containerClass,
+  imageClass,
+  widths,
+  sizes,
+  formats
+) => {
+  if (!src) {
+    errorSrcRequired('image');
+  }
+  return processImage({
+    src,
+    alt,
+    caption,
+    loading,
+    containerClass,
+    imageClass,
+    widths,
+    sizes,
+    formats
+  });
+};
+
+// Named parameters
+export const imageKeys = async (options = {}) => {
+  if (!options.src) {
+    errorSrcRequired('imageKeys');
+  }
+  return processImage(options);
 };